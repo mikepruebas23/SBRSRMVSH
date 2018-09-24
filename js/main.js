a8cbcb0c198b31cf72034fbd838df784dd4c4d3e
Miguel Angel <mikepruebas23@gmail.com>
2018-09-24 08:31:28 -0600

    agregando comentarios

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.up').hide();
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+      $('.up').fadeIn('1000');
+    }
+    else
+    {
+      $('.up').fadeOut('1000');
+    }
+});
+
+$('.up').click(function(){
+  $('body, html').animate({
+    scrollTop: 0
+  },450)
+});
+});
