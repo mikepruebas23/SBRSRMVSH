49ee34a48878b7c891920b7ba65aab0a013f6f61
Miguel Angel <mikepruebas23@gmail.com>
2018-09-25 17:25:47 -0600

    cambios en movile desing -v0.3 anadiendo corazon

--- js/corazon.js
+++ js/corazon.js
@@ -1,7 +1,17 @@
+const $like = document.getElementById('like');
+var contador = 12;
+var pLike = document.getElementById('like-puntos');
+$like.addEventListener('click', (event) => {
+    // console.log(event);
+    // $like.classList.add('is-liked');
+    $like.classList.toggle('is-liked');
 
-    const $like = document.getElementById('like');
-    $like.addEventListener('click', (event) => {
-        // console.log(event);
-        // $like.classList.add('is-liked');
-        $like.classList.toggle('is-liked');
-    });
+    //contador likes
+    if ($like.classList == 'like') {
+        contador--;
+        pLike.innerHTML = 'Me Gusta ' + contador;
+    } else if ($like.classList == 'like is-liked') {
+        contador++;
+        pLike.innerHTML = 'Me Gusta ' + contador;
+    }
+});
--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-$(document).ready(function(){
-  $('.up').hide();
+$(document).ready(function() {
+    $('.up').hide();
 
-  $(window).scroll(function(){
-    if($(this).scrollTop() > 100){
-      $('.up').fadeIn('1000');
-    }
-    else
-    {
-      $('.up').fadeOut('1000');
-    }
-});
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.up').fadeIn('1000');
+        } else {
+            $('.up').fadeOut('1000');
+        }
+    });
 
-$('.up').click(function(){
-  $('body, html').animate({
-    scrollTop: 0
-  },450)
-});
+    $('.up').click(function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 450)
+    });
 });
